d57a1244aa7df776ed3d2d0a87f28a7d940ed871
Aandreyjiwoo <andrey.quintela@unc.edu.ph>
2024-12-26 02:29:11 +0800

    Add files via upload

---
+++ Insert_Category.sql
@@ -0,0 +1,8 @@
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (1, 'FOOD SUPPLEMENTS', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (2, 'HERBAL COFFEE MIX', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (3, 'MEDICAL SUPPLY / DEVICE', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (4, 'MEDICINE', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (5, 'MEDINE', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (6, 'PILLS', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (7, 'SOLUTION FOR INJECTION', NULL);
+INSERT INTO Category (CategoryID, CategoryName, Description) VALUES (8, 'SUPPLY', NULL);
